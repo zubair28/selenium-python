06e41e623f031c2881301e54192dfc584de6a36e
Mohammed Ali <zubair@Mohammeds-MacBook-Pro.local>
2020-01-08 18:20:16 -0500

    modified redcard testcase

--- sel-targetbuy.py
+++ sel-targetbuy.py
@@ -5,3 +5,6 @@ import time
 driver = webdriver.Chrome(executable_path="/Users/zubair/Downloads/chromedriver")
 driver.get("http://www.target.com")
 assert "Target : Expect More. Pay Less." in driver.title
+
+driver.find_element_by_xpath("//*[@data-test='headerUtilityNav']//a[@href='/redcard/about']").click()
+driver.find_element_by_xpath("").click()
